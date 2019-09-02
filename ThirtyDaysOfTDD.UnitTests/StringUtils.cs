873cf8f0be75222a8c7b3ff8d6f8eafe8d4c82e9
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-02 12:12:18 -0500

    Day Four setup.

---
+++ ThirtyDaysOfTDD.UnitTests/StringUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    public class StringUtils
+    {
+        public int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ThirtyDaysOfTDD.UnitTests/StringUtilsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+
+            var sentenceToScan = "TDD is awesome!";
+
+            var characterToScanFor = "e";
+
+            var expectedResult = 2;
+
+            var stringUtils = new StringUtils();
+
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
