5690728ed8b2723fa1f937aef0d11f5655ceda01
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-30 12:48:27 -0500

    Dependency Injection Example

---
+++ ThirtyDaysOfTDD.UnitTests/DependecyInjectionPractice/BusinessService-Improved.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysOfTDD.UnitTests.DependecyInjectionPractice
+{
+    public class BusinessService_Improved
+    {
+        private DataStoreProvider _dataStoreProvider;
+        private LogginProvider _logginProvider;
+        private WebServiceProvider _webServiceProvider;
+
+        public BusinessService_Improved(DataStoreProvider dataStoreProvider, 
+            LogginProvider logginProvider, WebServiceProvider webServiceProvider)
+
+        {
+            _dataStoreProvider = dataStoreProvider;
+            _logginProvider = logginProvider;
+            _webServiceProvider = webServiceProvider;
+        }
+
+    }
+
+
+    public class DataStoreProvider
+    {
+
+    }
+
+    public class LogginProvider
+    {
+
+    }
+
+    public class WebServiceProvider
+    {
+
+    } 
+}
