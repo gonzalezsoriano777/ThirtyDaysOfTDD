31d2c49d176ec13ef91ca31fb53700463c8e63e7
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-30 12:06:01 -0500

    Working On Dependency Injection Examples.

---
+++ ThirtyDaysOfTDD.UnitTests/Dependecy Injection Practice/BusinessService-GoodExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysOfTDD.UnitTests.Dependecy_Injection_Practice
+{
+    class BusinessService_GoodExample
+    {
+    }
+}
